d798f1ff8e22332a1e11093d191850d1a3efda17
Вадим <Vadimk89@mail.ru>
2021-11-29 21:49:56 +0500

    Добавьте файлы проекта.

---
+++ 11TskStruct/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11TskStruct
+{
+    public struct Solve
+    {
+        public double k, b;
+        public void Root()
+        {
+            if (k > 0 || k < 0)
+            {
+                Console.WriteLine("Значение числа х= {0}", (0 - b) / k);
+            }
+            else
+            {
+                Console.WriteLine("Деление на 0. Уравнение не решено");
+            }
+        }
+    }
+}
